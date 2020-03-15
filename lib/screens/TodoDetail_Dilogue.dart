1e3e5d1f2bf4087a64b33d8f321eaee3a57b8e2c
uzairleo <uzair.jan336@gmail.com>
2020-03-16 01:56:45 +0500

    Improving ux

--- lib/screens/TodoDetail_Dilogue.dart
+++ lib/screens/TodoDetail_Dilogue.dart
@@ -10,6 +10,9 @@ class TdoDetailDilogue extends StatefulWidget {
 class _TdoDetailDilogueState extends State<TdoDetailDilogue> {
   var priorities=["High","Low"];
   var _myCustomStyle=TextStyle(color:Colors.black54);
+  var titleController=TextEditingController();
+  var discriptionController=TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return myCustomDilogue();
@@ -21,43 +24,80 @@ class _TdoDetailDilogueState extends State<TdoDetailDilogue> {
    return AlertDialog(
         // shape: ,
         elevation: 2.0,
-        title: Text("New Note"),
+        title: Text("New task"),
         content: Container(
-          height: 250,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          height: 300,
+          child: ListView(
             children: <Widget>[
-              Row(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(padding: const EdgeInsets.only(top:8.0,left:4.0),
-             child:DropdownButton(
-               items: priorities.map((dropdownitem){
-                 return DropdownMenuItem(
-                   value: dropdownitem,
-                   child: Text(dropdownitem));
-               }).toList(), 
-               onChanged: (changedValue){
-                 setState(() {
-                   
-                 });
-               },
-               value: "Low",
-               )),
-               Padding(padding: const EdgeInsets.only(top:8.0,left:40.0),
-               child:Text("OR"),),
-               Padding(padding: const EdgeInsets.only(top:5.0,left:20.0),
-               child:Row(children: <Widget>[
-                // IconButton(icon: Icon(FontAwesomeIcons.squareFull,color: Colors.yellow,), onPressed: (){}),
-                colorContainer(20.0, 20.0, Colors.yellow),
-                SizedBox(width:20.0),
-                colorContainer(20.0, 20.0, Colors.red)
-                
-               ],))
+                  Row(
+                    children: <Widget>[
+                      Padding(padding: const EdgeInsets.only(top:8.0,left:4.0),
+                 child:DropdownButton(
+                   items: priorities.map((dropdownitem){
+                     return DropdownMenuItem(
+                       value: dropdownitem,
+                       child: Text(dropdownitem));
+                   }).toList(), 
+                   onChanged: (changedValue){
+                     setState(() {
+                       
+                     });
+                   },
+                   value: "Low",
+                   )),
+                   Padding(padding: const EdgeInsets.only(top:8.0,left:40.0),
+                   child:Text("OR"),),
+                   Padding(padding: const EdgeInsets.only(top:5.0,left:20.0),
+                   child:Row(children: <Widget>[
+                    // IconButton(icon: Icon(FontAwesomeIcons.squareFull,color: Colors.yellow,), onPressed: (){}),
+                    colorContainer(20.0, 20.0, Colors.yellow),
+                    SizedBox(width:20.0),
+                    colorContainer(20.0, 20.0, Colors.red)
+                    
+                   ],))
+                    ],
+                  ),
+                  Padding(padding: const EdgeInsets.only(top:10.0,left:0.0),
+                  child:Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    
+                    children:<Widget>[
+                      Text("Time",style:_myCustomStyle),
+
+                      
+                      
+                    ]
+                  )),
+                    Padding(padding: const EdgeInsets.only(top:10.0,left:0.0),
+                  child:TextField(
+                    controller: titleController,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: "Task title",
+                      hintStyle: TextStyle(color:Colors.grey),
+                      border: InputBorder.none
+                    ),
+                  ),),
+                   Padding(padding: const EdgeInsets.only(top:10.0,left:0.0),
+                  child:TextField(
+                    controller: discriptionController,
+                    keyboardType: TextInputType.text,
+                    textAlign: TextAlign.left,
+                    // enabled: false,
+                    decoration: InputDecoration(
+                      hintText: "Discription",
+                      hintStyle: TextStyle(color:Colors.grey),
+                      border: InputBorder.none
+                    ),
+                  ),),
+                  
+                 
                 ],
               ),
-              Padding(padding: const EdgeInsets.only(top:10.0,left:0.0),
-              child:Text("Title",style: _myCustomStyle),)
-             
             ],
           ),
         ),
@@ -79,5 +119,16 @@ colorContainer(_width,_height,_color)
       ),
     ),
   );
+}
+ selectTime()
+
+{
+  setState(() {
+    showTimePicker(
+    context: context, 
+    initialTime : TimeOfDay.now());
+  });
+   
+
 }
 }
